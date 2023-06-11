d337867ee43ebe11ea6841167f15a8085dfd96a9
Artem Molchanov <artem700@gmail.com>
2023-06-11 20:51:18 -0300

    added tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -5,7 +5,132 @@ import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
 
-    @Test
+    @Test //не проходит
+    public void shouldIllArgExceptNegativeInitialBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    -2_000,
+                    0,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test //не проходит
+    public void shouldIllArgExceptNegativeMinBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    2_000,
+                    -1_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+
+    @Test //не проходит
+    public void shouldIllArgExceptNegativeMaxBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    2_000,
+                    1_000,
+                    -10_000,
+                    5
+            );
+        });
+    }
+
+    @Test //не проходит
+    public void shouldIllArgExceptNegativeMaxBalanceIsLowerMinBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    2_000,
+                    12_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+
+    @Test //не проходит
+    public void shouldIllArgExceptInitialBalanceIsLowerMinBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    1_000,
+                    2_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test //не проходит
+    public void shouldIllArgExceptInitialBalanceIsMoreMaxBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    11_000,
+                    2_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+
+    @Test //проходит
+    public void shouldIllArgExceptRate() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    2_000,
+                    0,
+                    10_000,
+                    -5
+            );
+        });
+    }
+
+    @Test //проходит
+    public void shouldPayOverMinBalance() {
+
+        SavingAccount account = new SavingAccount(
+                2_000,
+                0,
+                10_000,
+                5
+        );
+        account.pay(1_000);
+
+        Assertions.assertEquals(1_000, account.getBalance());
+
+    }
+
+    @Test //не проходит
+    public void shouldPayLowerMinBalance() {
+
+        SavingAccount account = new SavingAccount(
+                2_000,
+                0,
+                10_000,
+                5
+        );
+        account.pay(3_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+
+    }
+
+
+    @Test //не проходит
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -13,9 +138,22 @@ public class SavingAccountTest {
                 10_000,
                 5
         );
-
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test //проходит
+    public void shouldCalcPercentYearChange() {
+        SavingAccount account = new SavingAccount(
+                200,
+                1_000,
+                10_000,
+                15
+        );
+
+
+        Assertions.assertEquals(30, account.yearChange());
+    }
+
 }
